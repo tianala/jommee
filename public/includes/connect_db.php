170a23ac3796ac3c79ab9ac743e9c0631f94c641
23jammy <23jammycruz@gmail.com>
2025-05-18 02:17:40 +0800

    Add connect_db.php

---
+++ public/includes/connect_db.php
@@ -0,0 +1,47 @@
+<?php
+function loadEnv($path)
+{
+	if (!file_exists($path)) {
+		throw new Exception("Environment file not found.");
+	}
+
+	$lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	foreach ($lines as $line) {
+		if (strpos(trim($line), '#') === 0) {
+			continue;
+		}
+
+		list($key, $value) = explode('=', $line, 2);
+		putenv("$key=$value");
+	}
+}
+
+
+
+
+loadEnv(__DIR__ . '/../.env');                          // ----- DEVELOPMENT  ----- //
+
+
+
+date_default_timezone_set("Asia/Manila");
+$charset = 'utf8mb4';
+
+
+$hostname = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$defaultSchema = getenv('DB_NAME');
+$charset = getenv('DB_CHARSET');
+
+$dsn = "mysql:host=$hostname;dbname=$defaultSchema;charset=$charset;port=$port";
+
+$option = [
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+	PDO::ATTR_EMULATE_PREPARES => false
+];
+
+global $pdo;
+$pdo = new PDO($dsn, $username, $password, $option);
+
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
+<?php 
+include_once 'includes/connect_db.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./assets/css/output.css">
+    <link rel="stylesheet" href="./assets/css/fontawesome/all.min.css">
+    <link rel="stylesheet" href="./assets/css/fontawesome/fontawesome.min.css">
+    <script src="./assets/js/jquery-3.7.1.min.js"></script>
 </head>
 <body class="">
     
